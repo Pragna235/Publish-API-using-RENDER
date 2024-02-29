aed05bf4057d6011afc44c5625c71e5aec2fb8fe
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-02-29 16:49:11 +0530

    Create users.js

---
+++ package/users.js
@@ -0,0 +1,41 @@
+const users = [
+
+    {
+    
+    id: 1,
+    
+    name: 'P Sai Kiran',
+    
+    email: 'psaikiran@gmail.com',
+    
+    type: 'admin'
+    
+    },
+    
+    {
+    
+    id: 2,
+    
+    name: 'Srinivas',
+    
+    email: 'vasu@gmail.com',
+    
+    status: 'guest'
+    
+    },
+    
+    {
+    
+    id: 3,
+    
+    name: 'Thrishank',
+    
+    email: 'thrishank@gmail.com',
+    
+    status: 'admin'
+    
+    }
+    
+    ];
+    
+    module.exports = users;
